f3601266f357a53a3b73deb7e2c723bc0820b92d
aditya-hansraj <hansrajaditya.me@gmail.com>
2025-02-10 20:55:49 +0530

    .

--- client/src/components/student/chatbot/MessageList.tsx
+++ client/src/components/student/chatbot/MessageList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Message } from '../../../types';
 import { MessageBubble } from './MessageBubble';
 
