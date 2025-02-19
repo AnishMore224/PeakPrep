bbf0281d0bdd4541eac174a13f06c0997f42271e
Anish More <118825433+AnishMore224@users.noreply.github.com>
2025-02-19 15:36:46 +0530

    Update ProfileView.tsx

--- client/src/components/ProfileView.tsx
+++ client/src/components/ProfileView.tsx
@@ -1,10 +1,12 @@
 import { Admin, Hr, Student } from "../contexts/auth.context.tsx";
+import { useNavigate } from "react-router-dom";
 
 interface ProfileViewProps {
   user: Student | Hr | Admin | null;
 }
 
 export const ProfileView = ({ user }: ProfileViewProps) => {
+  const navigate = useNavigate();
   return (
     <div className="bg-gray-200 rounded-3xl p-8">
       <div className="flex items-center gap-4 mb-8">
@@ -85,11 +87,11 @@ export const ProfileView = ({ user }: ProfileViewProps) => {
             <ProfileField label="Email" value={(user as Admin).email} />
             <button
               className="w-full mt-4 py-2 px-4 text-white font-semibold rounded-lg 
-                   bg-gradient-to-r from-blue-500 to-indigo-600 shadow-md 
-                   hover:from-blue-600 hover:to-indigo-700 
-                   focus:ring-2 focus:ring-blue-300 
-                   transition-all duration-300"
-              onClick={() => (window.location.href = "/change-password")}
+                 bg-gradient-to-r from-blue-500 to-indigo-600 shadow-md 
+                 hover:from-blue-600 hover:to-indigo-700 
+                 focus:ring-2 focus:ring-blue-300 
+                 transition-all duration-300"
+              onClick={() => navigate("/change-password")}
             >
               Change Password
             </button>
